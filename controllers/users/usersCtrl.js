2d22ada108d6ba4d39d62f3553dfacb7a4db8689
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-11-16 10:35:45 +0530

    Implement `registerUserCtrl` to handle user registration

---
+++ controllers/users/usersCtrl.js
@@ -0,0 +1,60 @@
+const logger = require("../../config/logger");
+const User = require("../../models/User/User");
+
+const registerUserCtrl = async (request, response) => {
+  const { username, password, email } = request.body;
+
+  if (!username || !password || !email) {
+    return response.status(400).json({
+      status: "fail",
+      message: "All fields (username, password, email) are required.",
+    });
+  }
+
+  try {
+    // Check if the user already exists
+    const userFound = await User.findOne({ username });
+
+    if (userFound) {
+      return response.status(409).json({
+        status: "fail",
+        message: "Username is already taken. Please choose a different one.",
+      });
+    }
+
+    // Create a new user instance
+    const newUser = new User({
+      username,
+      email,
+      password,
+    });
+
+    // Save the new user to the database
+    await newUser.save();
+
+    // Respond with success message
+    return response.status(201).json({
+      status: "success",
+      message: "User registered successfully.",
+      data: {
+        _id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+        role: newUser.role,
+      },
+    });
+  } catch (error) {
+    logger.error(`Error in registering the user: ${error.message}`);
+
+    return response.status(500).json({
+      status: "error",
+      message:
+        "An unexpected error occurred while processing your request. Please try again later.",
+      error: process.env.NODE_ENV === "production" ? undefined : error.message, // Hiding error details in production
+    });
+  }
+};
+
+module.exports = {
+  registerUserCtrl,
+};
--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const logger = require("./config/logger");
-const Post = require("./models/Post/Post");
 const dbConnect = require("./config/dbConnect");
 
 dotenv.config();
@@ -13,27 +12,6 @@ const app = express();
 
 // * Middleware's
 app.use(express.json());
-
-// Create post route
-app.post("/api/v1/posts/create", async (request, response) => {
-  try {
-    const { title, content } = request.body;
-    const postCreated = await Post.create({ title, content });
-
-    return response.status(201).json({
-      status: "success",
-      message: "Post created successfully",
-      data: postCreated,
-    });
-  } catch (error) {
-    logger.error(`Error creating post: ${error.message}`);
-    return response.status(500).json({
-      status: "error",
-      message: "An error occurred while creating the post",
-      error: error.message,
-    });
-  }
-});
 
 // Use PORT from environment variables or default to 2027
 const PORT = process.env.PORT || 2027;
