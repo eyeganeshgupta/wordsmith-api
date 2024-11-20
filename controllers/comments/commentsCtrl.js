da07a06acd53ca9ea0410334f8e4bb70619bc63e
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-11-20 10:28:47 +0530

    update and delete comment controller

--- controllers/comments/commentsCtrl.js
+++ controllers/comments/commentsCtrl.js
@@ -3,11 +3,18 @@ const Comment = require("../../models/Comment/Comment");
 const Post = require("../../models/Post/Post");
 
 // @desc Create a Comment
-// @route POST /api/v1/comments/:postId
+// * @route POST /api/v1/comments/:postId
 // @access Private
 const createCommentCtrl = asyncHandler(async (request, response) => {
   const { content } = request.body;
   const { postId } = request.params;
+
+  // Validate input
+  if (!content) {
+    const error = new Error(`Comment content is required.`);
+    error.responseStatusCode = 400;
+    throw error;
+  }
 
   const comment = await Comment.create({
     content,
@@ -21,14 +28,72 @@ const createCommentCtrl = asyncHandler(async (request, response) => {
     { new: true }
   );
 
-  // Respond with success message
   return response.status(201).json({
     status: "success",
-    message: "Comment created successfully.",
+    message: "Comment has been successfully created.",
     data: comment,
+  });
+});
+
+// @desc Update a Comment
+// * @route PUT /api/v1/comments/:id
+// @access private
+const updateCommentCtrl = asyncHandler(async (request, response) => {
+  const { id } = request.params;
+  const { content } = request.body.content;
+
+  // Validate input
+  if (!content) {
+    const error = new Error(`Comment content is required.`);
+    error.responseStatusCode = 400;
+    throw error;
+  }
+
+  const updatedComment = await Post.findByIdAndUpdate(
+    id,
+    { content: content },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  if (!updatedComment) {
+    const error = new Error(`No comment found with the specified ID: ${id}.`);
+    error.responseStatusCode = 404;
+    throw error;
+  }
+
+  return response.status(200).json({
+    status: "success",
+    message: `Comment updated successfully with content: ${updatedComment?.content}.`,
+    data: updatedComment,
+  });
+});
+
+// @desc Delete a Comment
+// * @route DELETE /api/v1/comments/:id
+// @access private
+const deleteCommentCtrl = asyncHandler(async (request, response) => {
+  const { id } = request.params;
+
+  const deletedComment = await Post.findByIdAndDelete(id);
+
+  if (!deletedComment) {
+    const error = new Error(`No comment found with the specified ID: ${id}.`);
+    error.responseStatusCode = 404;
+    throw error;
+  }
+
+  return response.status(200).json({
+    status: "success",
+    message: "Comment has been successfully deleted.",
+    data: deletedComment,
   });
 });
 
 module.exports = {
   createCommentCtrl,
+  updateCommentCtrl,
+  deleteCommentCtrl,
 };
--- routes/comments/commentRouter.js
+++ routes/comments/commentRouter.js
@@ -2,10 +2,16 @@ const express = require("express");
 const isLoggedIn = require("../../middlewares/isLoggedIn");
 const {
   createCommentCtrl,
+  updateCommentCtrl,
+  deleteCommentCtrl,
 } = require("../../controllers/comments/commentsCtrl");
 
 const commentRouter = express.Router();
 
 commentRouter.post("/:postId", isLoggedIn, createCommentCtrl);
+
+commentRouter.put("/:id", isLoggedIn, updateCommentCtrl);
+
+commentRouter.delete("/:id", isLoggedIn, deleteCommentCtrl);
 
 module.exports = commentRouter;
