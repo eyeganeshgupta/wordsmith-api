70169c166732d201705e09fd77e322a7b8b2c290
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-04-09 20:14:52 +0530

    fixed profile picture on the navbar

--- controllers/users/usersCtrl.js
+++ controllers/users/usersCtrl.js
@@ -108,6 +108,7 @@ const loginUserCtrl = asyncHandler(async (request, response) => {
     },
     // Generate a JWT token
     token: generateToken(userFound),
+    profilePicture: userFound?.profilePicture,
   });
 });
 
